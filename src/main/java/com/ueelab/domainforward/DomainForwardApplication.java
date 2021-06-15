3d3a99fda1d06c1592fd89ed029edd80d64cff5c
Ueelab <S.Flora@msn.com>
2021-06-15 15:27:12 +0800

    Add files via upload

---
+++ src/main/java/com/ueelab/domainforward/DomainForwardApplication.java
@@ -0,0 +1,16 @@
+package com.ueelab.domainforward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ueelab
+ */
+@SpringBootApplication
+public class DomainForwardApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DomainForwardApplication.class, args);
+    }
+    
+}
---
+++ src/main/java/com/ueelab/domainforward/ForwardController.java
@@ -0,0 +1,36 @@
+package com.ueelab.domainforward;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+/**
+ * @author ueelab
+ */
+@Controller
+public class ForwardController {
+    
+    @Value("${resources-path}")
+    private String resourcesPath;
+    
+    @GetMapping("/")
+    public void home(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        URL url = new URL(request.getRequestURL().toString());
+        System.out.println(url.getHost());
+        File file = new File(resourcesPath + url.getHost() + ".png");
+        InputStream inputStream = new FileInputStream(file);
+        OutputStream outputStream = response.getOutputStream();
+        inputStream.transferTo(outputStream);
+        response.setContentType("image/png");
+    }
+    
+}
