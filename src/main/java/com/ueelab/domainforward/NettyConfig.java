f9432e8b8cb4190e353860f112aef47b5a05898e
ueelab <S.Flora@msn.com>
2022-05-23 18:11:37 +0800

    improve: Refactoring with webflux

---
+++ src/main/java/com/ueelab/domainforward/NettyConfig.java
@@ -0,0 +1,46 @@
+package com.ueelab.domainforward;
+
+import lombok.SneakyThrows;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+
+@Configuration
+public class NettyConfig {
+    
+    @Bean
+    public ErrorWebExceptionHandler exceptionHandler() {
+        return (exchange, ex) -> {
+            exchange.getResponse().setStatusCode(HttpStatus.OK);
+            return Mono.empty();
+        };
+    }
+    
+    @PostConstruct
+    public void startRedirectServer() {
+        new NettyReactiveWebServerFactory(80).getWebServer((request, response) -> {
+            URI uri = this.newUri(request.getURI());
+            response.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+            response.getHeaders().setLocation(uri);
+            return response.setComplete();
+        }).start();
+    }
+    
+    @SneakyThrows
+    private URI newUri(URI uri) {
+        return new URI("https",
+                uri.getUserInfo(),
+                uri.getHost(),
+                443,
+                uri.getPath(),
+                uri.getQuery(),
+                uri.getFragment());
+    }
+    
+}
